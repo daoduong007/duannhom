5d26b8595509b2a8b13b57376e1770622d8051ea
SuManucianS <vuvansu198@gmail.com>
2020-11-24 00:27:21 +0700

    duong_update

---
+++ WebApplication3/Models/DAL/NhanVienDAL.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication3.Models.DAL
+{
+	public class NhanVienDAL
+	{
+		//file này để thực hiện các thao tác với database ở đây là bảng nhân viên
+	}
+}
---
+++ WebApplication3/Models/Entity/NhanVien.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication3.Models.Entity
+{
+	public class NhanVien
+	{
+		//file này để ánh xạ bảng nhân viên ra thành 1 đối tượng 
+	}
+}
